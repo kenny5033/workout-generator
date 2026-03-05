a58effc65d6c7dbc107c6cf3969e2a45c345cef1
Kenneth Howes <kenneth.howes53@gmail.com>
2026-03-05 10:02:59 -0500

    more robust client setup

--- client/src/routes/+page.ts
+++ client/src/routes/+page.ts
@@ -1,6 +1,7 @@
 import type { PageLoad } from './$types';
+import { PUBLIC_API_PROTOCOL, PUBLIC_API_HOSTNAME, PUBLIC_API_PORT } from '$env/static/public';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const res = await fetch('http://localhost:8888');
+	const res = await fetch(`${PUBLIC_API_PROTOCOL}://${PUBLIC_API_HOSTNAME}:${PUBLIC_API_PORT}`);
 	return await res.json();
 };
